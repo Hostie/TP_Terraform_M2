72304bc4027718f75a6b9ba4588413e02771f4b8
Thibsito <93706432+Thibsito@users.noreply.github.com>
2023-07-05 17:59:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ variable psw {}
 
 resource "random_password" "password" {
   length           = 16
-  special          = true
-  override_special = "!#$%&"
+  special          = false
 }
 
 resource "null_resource" "ssh_target" {
@@ -19,11 +18,11 @@ resource "null_resource" "ssh_target" {
 
     provisioner "remote-exec" {
         inline = [
-            "echo ${var.psw} | sudo -S mkdir /home/server/openstack",
-            "sudo chown server:server /home/server/openstack",
-            "sudo chmod +x /home/server/openstack",
-            "echo 'server ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/server",
-            "cd /home/server/openstack",
+            "echo ${var.psw} | sudo -S mkdir /opt/stack",
+            "sudo chown server:server /opt/stack",
+            "sudo chmod +x /opt/stack",
+            "echo 'server ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/stack",
+            "cd /opt/stack",
             "git clone https://opendev.org/openstack/devstack",
             "cd devstack",
             "cat > local.conf << EOF\n[[local|localrc]]\nADMIN_PASSWORD=${random_password.password.result}\nDATABASE_PASSWORD=${random_password.password.result}\nRABBIT_PASSWORD=${random_password.password.result}\nSERVICE_PASSWORD=${random_password.password.result}\nEOF",
